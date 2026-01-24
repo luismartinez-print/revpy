01a5e58c7c1f52726bc662a3842296d49edb24b1
luismartinez-print <luismmartinez22335@gmail.com>
2026-01-24 12:40:18 -0500

    added some logic for network management

--- src/rmpy/models/airline.py
+++ src/rmpy/models/airline.py
@@ -2,6 +2,7 @@ from .base import Resource
 from dataclasses import dataclass, field
 from typing import List
 from operator import attrgetter
+from itertools import permutations
 
 @dataclass
 class FareClass:
@@ -40,3 +41,37 @@ class Leg(Resource):
     @property
     def name(self) -> str:
         return f'{self.origin} - {self.destination}'
+    
+
+### Network Logic
+
+class HubNetwork:
+    """
+    Class that creates a hub and spoke network to run optimizations
+    """
+
+    def __init__(self, hub_code: str):
+        self.hub_code = hub_code
+        self.spokes = []
+        self.legs = {}
+        self.itineraries = []
+
+    def add_spoke(self, city_code: str, capacity: int):
+        """
+        Docstring for add_spoke
+
+        Adds an airport or city spoke and automatically creates the two legs necessary
+        1. Inbound
+        2. Outbound
+        
+        """
+        self.spokes.append(city_code)
+        leg_in = Leg(origin = city_code, destination=self.hub_code, capacity=capacity)
+        self.legs[f"{city_code} - {self.hub_code}"] = leg_in
+
+        leg_out = Leg(origin=self.hub_code, destination=city_code, capacity=capacity)
+        self.legs[f"{self.hub_code} - {city_code}"] = leg_out
+
+        def generate_fares(): 
+            pass #comeback to this one
+    
